e4903ba6bbae90ecaa7eb2b9e5706e2f9459cb90
Ashok Govinda Gowda <8asokagowda@gmail.com>
2017-10-01 17:01:47 -0500

    Listen And Write functionality MessagingBoardApplication
    This Commit is for the listening and write functionality of message board application

--- P1T2/messageboard.py
+++ P1T2/messageboard.py
@@ -3,80 +3,45 @@ dir(redis)
 
 r = redis.Redis()
 boardName=""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+subscribing = False;
+publisherSubscribe=1
 
 def performSelect(command):
+    global boardName
     splitCommand=command.split()
     if len(splitCommand)!=2:
         print("\nInsufficient number of parameters for the command select \n Syntax select <board name>")
     else:
         boardName=splitCommand[1]
-        r.client_setname(username)
 
 
 def performRead(command):
+    global boardName,subscribing,publisherSubscribe
     splitCommand = command.split()
     if len(splitCommand)!=1:
         print("\nInsufficient number of parameters for the command read \n Syntax 'read'")
     elif boardName=="":
-        print("\n No board was selected before run command was issued")
+        print("\n No board was selected before read command was issued")
     else:
-        messages=r.get("messages")
+        messages=r.get(boardName)
         for message in messages:
             print("\n"+ message)
 
 
 
 
-
-
-
-
 def performListen(command):
-    pass
-
-
-
-
+    global boardName, subscribing, publisherSubscribe
+    splitCommand = command.split()
+    if len(splitCommand) != 1:
+        print("\nInsufficient number of parameters for the command listen \n Syntax 'listen'")
+    elif boardName == "":
+        print("\n No board was selected before listen command was issued")
+    else:
+        subscribing=True
+        publisherSubscribe=r.pubsub()
+        res = publisherSubscribe.subscribe(boardName)
+        print(res)
 
 
 
@@ -93,61 +58,40 @@ def performStop(command):
 
 
 def performWrite(command):
-    pass
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    global boardName, subscribing, publisherSubscribe
+    splitCommand = command.split()
+    if boardName == "":
+        print("\n No board was selected before write command was issued")
+    else:
+        message=username+":"+" ".join(splitCommand[1:])
+        r.publish(boardName,message)
+        if r.exists(boardName):
+            r.get(boardName)
+            r.rpush(boardName,message)
+        else:
+            r.rpush(boardName,message)
 
 
 print("\nEnter the Username")
 username=input()
 while True:
-    command=input()
-    if command.__contains__("select"):
-        performSelect(command)
-    elif command.__contains__("read"):
-        performRead(command)
-    elif command.__contains__("write"):
-        performWrite(command)
-    elif command.__contains__("listen"):
-        performListen(command)
-    elif command.__contains__("stop"):
-        performStop(command)
-    else:
-        print("Command does not exist")
-
+    try:
+        if subscribing:
+            print("Sub")
+            for item in publisherSubscribe.listen():
+                print(item)
+        command=input()
+        if command.__contains__("select"):
+            performSelect(command)
+        elif command.__contains__("read"):
+            performRead(command)
+        elif command.__contains__("write"):
+            performWrite(command)
+        elif command.__contains__("listen"):
+            performListen(command)
+        elif command.__contains__("stop"):
+            performStop(command)
+        else:
+            print("Command does not exist")
+    except KeyError as e:
+        performStop("stop")
